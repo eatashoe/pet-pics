97df35dd7b78161cbeca8d89ccf9243ec496b434
Winston <winston@BananaBook.local>
2021-10-26 01:23:09 -0400

    removed counter

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import pictureReducer from '../components/pictureSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     picture: pictureReducer,
   },
 });
